36e2018b6f29686bd044d6a1288775b307213fbc
techbabette <marinakrsticrf@gmail.com>
2024-03-24 16:03:36 +0100

    Temporary loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,43 @@ import { Provider } from 'react-redux'
 
 import ToastContainer from './toast/ToastContainer'
 
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+function load(){
+    return new Promise((Resolve, Reject) => {
+        setTimeout(() => Resolve("done"), 1000);
+    });
+};
+
 function App(){
+    let [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        async function initialLoad() {
+            let response = await load();
+            setIsLoaded(true);
+        }
+
+        initialLoad();
+    }, []);
+
+    let loading = 
+    <div className='w-full h-screen mk-text-center'>
+        <p className='my-2 text-8xl text-primary'>Formify</p>
+        <span className="loading loading-spinner mx-auto loading-lg"></span>
+    </div>
+
     return (
     <Provider store={store}>
-        <RouterProvider router={router}/>
-        <ToastContainer/>
+        {isLoaded && 
+        <>
+            <RouterProvider router={router}/>
+            <ToastContainer/>
+        </>
+        }
+        {!isLoaded && loading
+        }
     </Provider>
     );
 }
