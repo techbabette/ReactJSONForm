5ac0c53f5173f3665ecf0f1fe577dfd49c4477b4
techbabette <marinakrsticrf@gmail.com>
2024-04-04 16:01:19 +0200

    [FEAT] Landing page includes call to action

--- src/PageWelcome.jsx
+++ src/PageWelcome.jsx
@@ -1,6 +1,16 @@
+import NavLink from "./NavLink";
 function PageWelcome(){
+
+    let callToAction = <NavLink className="btn btn-secondary w-1/2 m-2" to={"/form/new"} text={"Create new form"}/>
+
     return (
-        "Welcome to Formify"
+        <div className="w-9/12 sm:w-6/12 mx-auto solo-page flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center items-center rounded-lg bg-base-200 p-10 border-4 border-secondary">
+                <p className="text-6xl bold">Welcome to Formify</p>
+                <p>Easy forms, for everyone!</p>
+                {callToAction}
+            </div>
+        </div>
     );
 }
 
