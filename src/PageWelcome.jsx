a1970963f15d32b97f26a69de514196aba711c16
techbabette <marinakrsticrf@gmail.com>
2024-04-04 16:03:17 +0200

    [FEAT] Smaller hero text size on phones

--- src/PageWelcome.jsx
+++ src/PageWelcome.jsx
@@ -6,7 +6,7 @@ function PageWelcome(){
     return (
         <div className="w-9/12 sm:w-6/12 mx-auto solo-page flex flex-col justify-center items-center">
             <div className="flex flex-col justify-center items-center rounded-lg bg-base-200 p-10 border-4 border-secondary">
-                <p className="text-6xl bold">Welcome to Formify</p>
+                <p className="text-4xl md:text-6xl bold text-center">Welcome to Formify</p>
                 <p>Easy forms, for everyone!</p>
                 {callToAction}
             </div>
