2f73bfade079d64271261ca5f1e1f99697adc19e
techbabette <marinakrsticrf@gmail.com>
2024-08-17 06:00:45 +0200

    [FEAT] Clickable reset form button

--- src/FormComplete.jsx
+++ src/FormComplete.jsx
@@ -26,6 +26,11 @@ function FormComplete(props){
         props.onSubmit(formValue);
     }
 
+    function handleReset(){
+        setFormValue({});
+        setFormErrors({});
+    }
+
     function removeError(key){
         let newErrors = {...formErrors};
         delete newErrors[key];
@@ -57,7 +62,10 @@ function FormComplete(props){
     <div className={'w-full ' + props.className}>
         <h1 className="p-2 text-2xl">{form.formName ? form.formName : "Form name goes here"}</h1>
         <FormInputs form={form} errors={formErrors} value={formValue} onChange={handleChange}/>
-        <button type="button" className="btn btn-secondary w-full px-2 my-5" onClick={handleSubmit}>Submit form</button>
+        {form.resetButtonAvailable === "true" && 
+        <button type="button" className="btn btn-accent w-full px-2 my-2" onClick={handleReset}>Reset form</button>
+        }
+        <button type="button" className="btn btn-secondary w-full px-2 my-2" onClick={handleSubmit}>Submit form</button>
     </div>
     );
 }
