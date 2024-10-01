695a2545caf404ba6cf639dc91dcb666ecac7548
techbabette <marinakrsticrf@gmail.com>
2024-10-01 12:00:32 +0200

    [FEAT] Pagination buttons on myforms page

---
+++ src/ButtonsPagination.jsx
@@ -0,0 +1,23 @@
+function ButtonsPagination(props){
+    let page = props.page;
+    let lastPage = props.lastPage;
+    let setPage = props.setPage;
+    
+    function changePage(direction){
+        setPage(page + direction);
+    }
+
+    return (
+    <>    
+    {(lastPage > 1) && 
+    <div className='w-full mx-auto flex flex-row items-center justify-between'>
+        <button disabled={page === 1} onClick={() => changePage(-1)} className="btn btn-neutral disabled:btn-disabled">&lt;</button>
+        <p>{page}</p>
+        <button disabled={page >= lastPage} onClick={() => changePage(1)} className="btn btn-neutral disabled:btn-disabled">&gt;</button>
+      </div>
+    }
+    </>
+    )
+}
+
+export default ButtonsPagination;
--- src/PageMyForms.jsx
+++ src/PageMyForms.jsx
@@ -2,21 +2,28 @@ import { useState, useEffect } from 'react';
 import axios from './axios/axios';
 import { useNavigate } from 'react-router-dom';
 import PageMyFormsForm from './PageMyFormsForm';
+import ButtonsPagination from './ButtonsPagination';
 function PageMyForms() {
   let [loaded, setLoaded] = useState(false);
   let [error, setError] = useState(false);
   let [forms, setForms] = useState(null);
   let [page, setPage] = useState(1);
+  let [lastPage, setLastPage] = useState(1);
   const navigate = useNavigate();
 
   useEffect(() => {
     getForms();
   }, [])
 
-  async function getForms(page = 1){
-    let result = await axios.get(`forms/me`, {page});
+  useEffect(() => {
+    getForms(page);
+  }, [page])
 
+  async function getForms(page = 1){
+    let result = await axios.get(`forms/me`, {params : {page}});
+    console.log(page);
     if(result.success){
+        setLastPage(result.data.body.last_page);
         setForms(result.data.body.data);
     }
     else{
@@ -26,18 +33,27 @@ function PageMyForms() {
 
   let formsHTML = forms?.map((form) => {
     return (
-        <PageMyFormsForm form={form}/>
+        <PageMyFormsForm form={form} key={form.id}/>
     )
   })
+
+  let pageControl = <ButtonsPagination page={page} setPage={setPage} lastPage={lastPage}/>
 
   return (
     <div className='w-full h-screen'>
         <p className='my-3 text-2xl mk-text-center text-primary'>These are your forms</p>
-        {(!forms && !error) && <span className="loading loading-spinner mk-text-center mx-auto loading-lg"></span>}
+        {(!forms && !error) && <span className="loading loading-spinner mk-text-center my-auto mx-auto loading-lg"></span>}
         {error && "Your forms could not be retrieved"}
         {(forms && forms.length === 0) && "No forms found"}
         {(forms && forms.length > 0) && 
-            formsHTML
+        <>
+        <div className='border-1 border-base-100 bg-base-100'>
+            {formsHTML}
+        </div>
+        <div className="w-6/12 mx-auto">
+        {pageControl}
+        </div>
+        </>
         }
     </div>
   )
