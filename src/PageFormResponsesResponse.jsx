b170bfe46df8537e7266097b1ec78247c72fb9f6
techbabette <marinakrsticrf@gmail.com>
2024-10-05 06:05:23 +0200

    [FIX] Add missing key attribute in loop

--- src/PageFormResponsesResponse.jsx
+++ src/PageFormResponsesResponse.jsx
@@ -4,7 +4,7 @@ function PageFormResponsesResponse(props){
     let responseValuesHTML = [];
     for(let valueKey of Object.keys(response.values)){
         let valueOfKey = response.values[valueKey] instanceof Array ? response.values[valueKey].join(", ") : response.values[valueKey];
-        responseValuesHTML.push(<p>{valueKey}: {valueOfKey}</p>)
+        responseValuesHTML.push(<p key={valueKey}>{valueKey}: {valueOfKey}</p>)
     }
     return (
         <div className='w-11/12 md:w-6/12 mx-auto flex flex-col flex-wrap border border-4 border-secondary bg-base-200 hover:bg-primary-content rounded-lg p-2 my-4 text-left'>
